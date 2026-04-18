12123eaf9ec70dfe96cda0bdc1316b9095d49e6b
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-04-18 09:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let firstName = documet.getElementById("firstname").value;
+let lastName = documet.getElementById("lastname").value;
+let phoneNumber = documet.getElementById("phonenumber").value;
+let email = documet.getElementById("email").value;
+let submit = document.getElementById("submit");
+
+submit.addEventListener("click",()=>{
+	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`);
+});
